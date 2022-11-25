3d727e151707c4f8f547e9f2bee1620f1c969b29
Jonas Castro <jonascastro.dev@gmail.com>
2022-11-25 09:24:16 -0300

    Primeiro commit, preparando o projeto.

---
+++ funetech/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+from django.contrib import admin
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
