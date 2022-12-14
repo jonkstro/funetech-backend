b56d9ed8b0581410600edd5e332393bb3fe35be5
Jonas Castro <jonascastro.dev@gmail.com>
2022-12-14 13:44:11 -0300

    projeto concluído

--- homenagem/models.py
+++ homenagem/models.py
@@ -10,7 +10,7 @@ class Homenagem(models.Model):
     data_falec = models.DateField(null=False, blank=False)
     cidade_falec = models.CharField(max_length=50, null=False, blank=False)
     memoria = models.TextField(max_length=255, null=True, blank=True)
-    biografia = models.TextField(max_length=800, null=True, blank=True)
+    biografia = models.TextField(max_length=5000, null=True, blank=True)
     foto = models.ImageField(upload_to='homenagem', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
