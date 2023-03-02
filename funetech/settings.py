dda1125dd03b3ea4b126769d778e140f703e29a4
Jonas Castro <jonascastro.dev@gmail.com>
2023-03-01 22:03:20 -0300

    database

--- funetech/settings.py
+++ funetech/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = 'funetech.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     },
+    {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
